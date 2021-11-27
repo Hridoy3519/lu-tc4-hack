e6c960c7cf9250f57a192eab4a0c8bdc6b3ef784
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-11-27 15:15:51 +0600

    Footer Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home/Home';
+import AuthProvider from './contexts/AuthProvider';
+import Login from './Pages/Authentication/Login/Login';
+import Register from './Pages/Authentication/Register/Register';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -19,6 +23,7 @@ function App() {
               <Register></Register>
             </Route>
           </Switch>
+          <Footer/>
         </Router>
       </AuthProvider>
     </div>

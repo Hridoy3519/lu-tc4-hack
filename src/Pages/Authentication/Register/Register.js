41ea9405aa078110ed16ccaf320a85ee69e40d1f
AR-Nasim <a.r.nasim5250@gmail.com>
2021-11-27 17:41:39 +0600

    Reg/Login page Done

--- src/Pages/Authentication/Register/Register.js
+++ src/Pages/Authentication/Register/Register.js
@@ -26,55 +26,73 @@ const Register = () => {
             alert('Your password did not match');
             return;
         }
-        loginData.name = loginData.fname.concat(" ",loginData.lname)
+        loginData.name = loginData.fname.concat(" ", loginData.lname)
         registerUser(loginData.email, loginData.phone, loginData.password, loginData.name, history);
         e.preventDefault();
     }
-    const handleGoogleSignIn = () => {
-        signInWithGoogle(location, history)
+    const handleChecked = e =>{
+        const userStatus = e.target.id;
+        console.log(userStatus);
+        const newLoginData = { ...loginData };
+        newLoginData["userStatus"] = userStatus;
+        setLoginData(newLoginData);
     }
 
     return (
         <>
-        <Header></Header>
-        <div style={{ height: '90vh' }} className="d-flex justify-content-center align-items-center bg margin">
-            <div className="form-container">
-                <div className="log-in-header">Sign Up</div>
-                <form >
-                    <div className="d-flex justify-content-between">
-                    <div className="form-floating mb-3 me-1 w-100 shadow-sm">
-                        <input onChange={handleOnBlur} type="text" className="form-control" name="fname" id="floatingName" placeholder="Name" required />
-                        <label htmlFor="floatingName">First Name</label>
-                    </div>
-                    <div className="form-floating mb-3 ms-1 w-100 shadow-sm">
-                        <input onChange={handleOnBlur} type="text" className="form-control" name="lname" id="floatingName" placeholder="Name" required />
-                        <label htmlFor="floatingName">Last Name</label>
-                    </div>
-                    </div>
-                    <div className="form-floating mb-3 shadow-sm">
-                        <input onChange={handleOnBlur} type="text" className="form-control" name="phone" id="floatingName" placeholder="Phone Number" required />
-                        <label htmlFor="floatingName">Phone Number</label>
-                    </div>
-                    <div className="form-floating mb-3 shadow-sm">
-                        <input onChange={handleOnBlur} type="email" className="form-control" id="floatingEmail" name="email" placeholder="name@example.com" required />
-                        <label htmlFor="floatingEmail">Email</label>
-                    </div>
-                    <div className="form-floating mb-3 shadow-sm">
-                        <input onChange={handleOnBlur} type="password" className="form-control" name="password" id="floatingPassword" placeholder="Password" required />
-                        <label htmlFor="floatingPassword">Password</label>
-                    </div>
-                    <div className="form-floating mb-3 shadow-sm">
-                        <input onChange={handleOnBlur} type="password" className="form-control" name="password2" id="floatingPassword" placeholder="Retype Password" required />
-                        <label htmlFor="floatingPassword">Confirm Password</label>
-                    </div>
-                    <button onClick={handleLoginSubmit} className="btn-custom">SignUp</button>
-                    <div className="mt-3">
-                        Already have an account? <NavLink to="/login">Login</NavLink>
-                    </div>
-                </form>
+            <Header></Header>
+            <div style={{ height: '90vh' }} className="d-flex justify-content-center align-items-center bg margin">
+                <div className="form-container">
+                    <div className="log-in-header">Sign Up</div>
+                    <form >
+                        <div className="d-flex justify-content-between">
+                            <div className="form-floating mb-3 me-1 w-100 shadow-sm">
+                                <input onChange={handleOnBlur} type="text" className="form-control" name="fname" id="floatingName" placeholder="Name" required />
+                                <label htmlFor="floatingName">First Name</label>
+                            </div>
+                            <div className="form-floating mb-3 ms-1 w-100 shadow-sm">
+                                <input onChange={handleOnBlur} type="text" className="form-control" name="lname" id="floatingName" placeholder="Name" required />
+                                <label htmlFor="floatingName">Last Name</label>
+                            </div>
+                        </div>
+                        <div className="form-floating mb-3 shadow-sm">
+                            <input onChange={handleOnBlur} type="text" className="form-control" name="phone" id="floatingName" placeholder="Phone Number" required />
+                            <label htmlFor="floatingName">Phone Number</label>
+                        </div>
+                        <div className="form-floating mb-3 shadow-sm">
+                            <input onChange={handleOnBlur} type="email" className="form-control" id="floatingEmail" name="email" placeholder="name@example.com" required />
+                            <label htmlFor="floatingEmail">Email</label>
+                        </div>
+                        <div className="form-floating mb-3 shadow-sm">
+                            <input onChange={handleOnBlur} type="password" className="form-control" name="password" id="floatingPassword" placeholder="Password" required />
+                            <label htmlFor="floatingPassword">Password</label>
+                        </div>
+                        <div className="form-floating mb-3 shadow-sm">
+                            <input onChange={handleOnBlur} type="password" className="form-control" name="password2" id="floatingPassword" placeholder="Retype Password" required />
+                            <label htmlFor="floatingPassword">Confirm Password</label>
+                        </div>
+                        <div className="text-start d-flex mb-2">
+                            <div class="form-check me-2">
+                                <input onChange={handleChecked} class="form-check-input radio-btn-color" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked />
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    Buyer
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input onChange={handleChecked} class="form-check-input radio-btn-color" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    Seller
+                                </label>
+                            </div>
+                        </div>
+                        <button onClick={handleLoginSubmit} className="btn-custom">SignUp</button>
+                        <div className="mt-3">
+                            Already have an account? <NavLink to="/login">Login</NavLink>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-</>
+        </>
     );
 };
 

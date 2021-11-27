0ee3ce3a68519343010096d2b7f519476252fb15
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-11-27 13:55:39 +0600

    Bug Fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './Pages/Authentication/Login/Login';
-import AuthProvider from './contexts/AuthProvider';
-import Register from './Pages/Authentication/Register/Register';
 import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <AuthProvider>
-        <Router>
-          <Switch>
-           <Route path='/' exact >
+    <Router>
+      <Switch>
+        <Route path='/' exact >
           <Home></Home>
-         </Route>
-            <Route path="/login">
-              <Login></Login>
-            </Route>
-            <Route path="/register">
-              <Register></Register>
-            </Route>
-          </Switch>
-        </Router>
-      </AuthProvider>
-    </div>
-    )
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
+
 export default App;

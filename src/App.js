bf84819598482bcee88332b7828bfd7142f00fab
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-11-27 15:39:37 +0600

    Footer removed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Pages/Home/Home/Home';
-import AuthProvider from './contexts/AuthProvider';
-import Login from './Pages/Authentication/Login/Login';
-import Register from './Pages/Authentication/Register/Register';
-import Footer from './Pages/Shared/Footer/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Pages/Home/Home/Home";
+import AuthProvider from "./contexts/AuthProvider";
+import Login from "./Pages/Authentication/Login/Login";
+import Register from "./Pages/Authentication/Register/Register";
+import Footer from "./Pages/Shared/Footer/Footer";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Switch>
-         <Route path='/' exact >
-        <Home></Home>
-       </Route>
+          <Route path="/" exact>
+            <Home></Home>
+            <Footer/>
+          </Route>
           <Route path="/login">
             <Login></Login>
           </Route>
@@ -22,7 +23,6 @@ function App() {
             <Register></Register>
           </Route>
         </Switch>
-        <Footer/>
       </Router>
     </AuthProvider>
   );

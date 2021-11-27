d05be7ab451ef759ebb7c23baf8aabf6234aa8af
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-11-27 13:09:40 +0600

    Home Product Card Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeProducts from './Pages/Home/HomeProducts/HomeProducts';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, Hridoy</h1>
+      <HomeProducts/>
     </div>
   );
 }

407d1e17be2029bbdb9fb679039b6650664bb86d
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-11-27 10:49:17 +0600

    Frist Commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_x3r-2e3bwhBTSOrR7XcT-WI-iwul5hM",
+    authDomain: "anti-plagiarism-7a4c1.firebaseapp.com",
+    projectId: "anti-plagiarism-7a4c1",
+    storageBucket: "anti-plagiarism-7a4c1.appspot.com",
+    messagingSenderId: "70757941394",
+    appId: "1:70757941394:web:7c4133fa7ea3e47e08264b"
+  };
+
+  export default firebaseConfig;

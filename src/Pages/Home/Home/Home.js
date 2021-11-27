681e972f8b55bba0bbd73ec963a824cb40e620bf
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-11-27 13:21:17 +0600

    Product card added

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../Shared/Navbar/Navbar';
+import Banner from '../Banner/Banner';
+import HomeProducts from '../HomeProducts/HomeProducts';
+
+const Home = () => {
+    return (
+        <>
+            <Header></Header>
+            <Banner></Banner>
+            <HomeProducts/>
+        </>
+    );
+};
+
+export default Home;
